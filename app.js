fe633f570d2ae4bf6758b7b84a9eaccb13d11d79
FremontTeng <fremontteng@gmail.com>
2020-10-03 19:08:04 +0800

    Added CDN for Axios

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Welcome to Top 100 Movies~'
+        //Default message to show
+        message: 'Welcome to Top 100 Movies~'
     },
     methods:{
 
     },
     computed:{
-        
+
     }
   })
