6e0c2f3828d47f304e12372887b264ba5b65304f
FremontTeng <fremontteng@gmail.com>
2020-10-03 20:42:08 +0800

    Add documentations to current codes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = new Vue({
         message: 'Welcome to Top 100 Movies~',
         //Data Received
         movies: [],
+        //API key that can be swapped
         api_key: "c886b1430121b5768bc22f4117e5cdc2"
     },
     methods:{
@@ -12,6 +13,7 @@ var app = new Vue({
         var app = this;
         //Use Axios to fetch the data
         axios.get('https://api.themoviedb.org/3/discover/movie?api_key='+app.api_key+'&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page='+PageNo)
+        //Runs the following after fetching the data
         .then(function (response){
             //Display app message when data is fetched
             app.message = "Fetching from page "+PageNo;
@@ -19,18 +21,25 @@ var app = new Vue({
             console.log(response.data.results);
             //For each movie found, save to list of movies
             for (movie in response.data.results){
-                //console.log(response.data.results[movie]);
                 app.movies.push(response.data.results[movie]);
             }
+            //Update the app message that data has been successfully fetched
             app.message = "Data successfully fetched!";
         })
+        //Catch any error
         .catch(function (error){
             //Log the error onto the page
             app.message = "Error occured: "+ error;
         })
         },
+
+        /**
+         * Function to fetch 100 movies from the button onclick
+         */
         loadMovies: function(){
             var app = this;
+            //Refresh the movies data, initialize the movies data into an empty array
+            app.movies = [];
             //Each page consists of 20 movies. Call 5 pages to call all 100 movies
             app.callAxios(1);
             app.callAxios(2);
