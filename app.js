b92a735206f80dc1f822072aa85abfaeadca32b2
FremontTeng <fremontteng@gmail.com>
2020-10-03 19:30:03 +0800

    Added Axios data response

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var app = new Vue({
         message: 'Welcome to Top 100 Movies~'
     },
     methods:{
-
+        loadMovies: function(){
+            var app = this;
+            //Use Axios to fetch the data
+            axios.get('https://api.themoviedb.org/3/discover/movie?api_key=c886b1430121b5768bc22f4117e5cdc2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+            .then(function (response){
+                //Display app message when data is fetched
+                app.message = response.data.results[0].title+": "+response.data.results[0].popularity;
+                //Display response data in console log
+                console.log(response.data.results);
+            })
+            .catch(function (error){
+                //Log the error onto the page
+                app.message = "Error occured: "+ error;
+            })
+            console.log();
+        }
     },
     computed:{
 
